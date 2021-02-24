1de40e8c77f775921567725204677e402284da77
BW <notanherbwright@gmail.com>
2021-02-23 19:46:34 -0700

    spelling

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-wget https://api.modpacks.ch/public/modpack/80/225/server/linuxls
+wget https://api.modpacks.ch/public/modpack/35/174/server/linux
 chmod +x linux./linux 35 --auto
 echo 'eula=true' > eula.txt
 mv forge-1.12.2-14.23.5.2846-universal.jar forge-1.12.2-14.23.5.2846.jar
