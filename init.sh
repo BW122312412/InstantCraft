301f11a4fea669751540ea07f51aad5b24db63f2
BW <notanherbwright@gmail.com>
2021-02-23 19:51:43 -0700

    i

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 wget https://api.modpacks.ch/public/modpack/35/174/server/linux
 chmod +x linux
-linux 35 --auto
+./linux 35 --auto
 echo 'eula=true' > eula.txt
 mv forge-1.12.2-14.23.5.2846-universal.jar forge-1.12.2-14.23.5.2846.jar
 add-apt-repository ppa:openjdk-r/ppa
